9fe6d570f27978791e3fd0a76b331427778c5ac2
anomalilies <takitafu8@gmail.com>
2020-10-26 16:43:14 +0000

    Updated Embeds

--- Commands/roleslistCommands.js
+++ Commands/roleslistCommands.js
@@ -1,12 +1,12 @@
-/*
+///*
 module.exports = async (client, id = []) => {
     const channel = await client.channels.fetch(id)
   
     channel.messages.fetch().then((messages) => {
 
-        if (messages.size === 2) {
+        if (messages.size === 0) {
             channel.send("<:spacer:757807111756447806>")
-            channel.send("**Other Roles**")
+            channel.send("**Roles List**")
             channel.send(roles1)
             channel.send("<:spacer:757807111756447806>")
             channel.send(roles2)
@@ -14,11 +14,13 @@ module.exports = async (client, id = []) => {
             channel.send(roles3)
             channel.send("<:spacer:757807111756447806>")
             channel.send(roles4)
+            channel.send("<:spacer:757807111756447806>")
+            channel.send("**Role Shop**")
         }
-        else channel.messages.fetch("769008744629141525")
+        /*else channel.messages.fetch("769008744629141525")
         .then(messages => {
             messages.edit(roles4);
-    });
+    });*/
     })
 }
-*/
+//*/
--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const botCommands = require("./Commands/botCommands");
 const roleslistCommands = require("./Commands/roleslistCommands");
 const ruleCommands = require("./Commands/ruleCommands");
 */
+const roleslistEmbeds = require("./Embeds/roleslistEmbeds");
+const roleslistCommands = require("./Commands/roleslistCommands");
 
 const ruleEmbeds = require("./Embeds/ruleEmbeds");
 var uwuifying = require("./UWU Translator/uwuify.js");
@@ -48,11 +50,11 @@ function statusChange() {
 client.once("ready", () => {
     console.log(`${client.user.tag} activated!`);
     setInterval(statusChange, 30000);
-
+    roleslistCommands(client, "758494476174884905");
     /*
     For creating/editing embeds:
     botCommands(client, "742548177462231120");
-    roleslistCommands(client, "758494476174884905");
+
     ruleCommands(client, "603248229928140801");
     */
 });
