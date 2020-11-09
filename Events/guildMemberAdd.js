2a0ad95dc92b0da2db5deb6d8a9b6ee056d30a56
anomalilies <takitafu8@gmail.com>
2020-11-09 21:03:56 +0000

    iamsleepy

--- Events/guildMemberAdd.js
+++ Events/guildMemberAdd.js
@@ -1,20 +1,10 @@
 const { emojis } = require("../config.json");
 
 module.exports = (client, member) => {
-    if (member.guild.id === "603246092402032670") {
-        member.guild.channels.cache.get("603246092402032673").send(emojis.wave);
+    if (member.guild.id === "706628883440468060") {
+        member.guild.channels.cache.get("775114107648475147").send(emojis.wave);
 
         var newbiesRole = member.guild.roles.cache.find(role => role.name === "Newbies");
         member.roles.add(newbiesRole);
-
-        if (Date.now() === member.joinedAt < 604800000) {
-            member.roles.remove(newbiesRole);
-        }
-    }
-    if (member.guild.id === "706628883440468060") {
-        var lurkersRole = member.guild.roles.cache.find(role => role.name === "Lurkers");
-        if (Date.now() === member.joinedAt > 604800000) {
-            member.roles.add(lurkersRole);
-        }
     }
 };
--- Events/ready.js
+++ Events/ready.js
@@ -11,7 +11,7 @@ const botCommands = require("../Embeds/Bots/botCommands");
 const contestCommands = require("../Embeds/Contests/contestCommands");
 const roleslistCommands = require("../Embeds/Roles/roleslistCommands");
 
-module.exports = (client) => {
+module.exports = async (client, member) => {
     console.log(`${client.user.tag} activated!`);
     function statusChange() {
         client.user.setActivity(data.statuses[Math.floor(Math.random() * data.statuses.length)], { type: "WATCHING" });
@@ -30,6 +30,34 @@ module.exports = (client) => {
         unknownCommand: false
     })
     .registerCommandsIn(path.join(__dirname, "../Commands"));
+
+    function checkNewbies() {
+        if (member.guild.id === "706628883440468060") {
+            var newbiesRole = member.guild.roles.cache.find(role => role.name === "Newbies");
+    
+            if (newbiesRole) {
+                if (Date.now() == member.joinedAt < 30000) {
+                    member.roles.remove(newbiesRole);
+                    console.log ("role removed")
+                }
+            else return;
+            }
+        }
+    };
+    function checkLurkers() {
+        if (member.guild.id === "706628883440468060") {
+            var lurkersRole = member.guild.roles.cache.find(role => role.name === "Lurkers");
+    
+            if (lurkersRole) {
+                if (Date.now() === member.joinedAt > 604800000) {
+                    member.roles.add(lurkersRole);
+                }
+                else return;
+            }
+        }
+    };
+    setInterval(checkLurkers, 3600000);
+    setInterval(checkNewbies, 30000);
 
     /*archiveCommands(client, "770726574865514517");
     botCommands(client, "742548177462231120");
