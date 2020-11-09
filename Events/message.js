a0a8a90cd14a3d667958c2dac4180cc3b7d4e63f
anomalilies <takitafu8@gmail.com>
2020-11-09 17:19:03 +0000

    Fixed -uwu in other servers

--- Events/message.js
+++ Events/message.js
@@ -5,6 +5,9 @@ const { MessageEmbed } = require("discord.js");
 const rules = require("../Embeds/ruleEmbeds.json");
 rules.forEach((rule, i) => rule.re = new RegExp(`(\\s|^)${prefix}${i+1}(\\s|$)`));
 
+var uwuifying = require("../Fun/UWU Translator/uwuify");
+var data = require("../Fun/UWU Translator/data");
+
 var whosTalkingWithPatPat = new Set();
 var fishyCommands = [
     "fishy", "fishytimer", "fishystats", "leaderboardfishy", "fish", "fihy", "fisy", "foshy", "fisyh", "fsihy", "fin",
