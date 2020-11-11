0f7bdd4bfc82bde16a358d6d7e2fd00f2745ec33
anomalilies <takitafu8@gmail.com>
2020-11-11 05:38:21 +0000

    embed creator

---
+++ Commands/Miscellaneous/affiliates.js
@@ -0,0 +1,45 @@
+const Commando = require("discord.js-commando");
+const { MessageEmbed } = require("discord.js");
+const { emojis } = require("../../config.json");
+const affiliatesChannel = "758082713885343844";
+
+module.exports = class AffiliatesCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name: "affiliate",
+            group: "misc",
+            memberName: "affiliate",
+            description: "Embed an affiliates link.",
+            args: [
+                {
+                    key: "title",
+                    prompt: "What would you like the title to be?",
+                    type: "string"
+                },
+                {
+                    key: "description",
+                    prompt: "What would you like the description to be?",
+                    type: "string"
+                },
+                {
+                    key: "link",
+                    prompt: "Paste the link here!",
+                    type: "string"
+                }
+            ],
+            ownerOnly: true
+        });
+    }
+
+    async run(message, { title, description, link }) {
+        const embed = new MessageEmbed()
+        .setTitle(title)
+        .setDescription(description)
+        .setColor(15849719);
+
+        const { guild } = message;
+        const channel = guild.channels.cache.get(affiliatesChannel);
+        channel.send(embed)
+        .then (channel.send(link))
+        .then (channel.send(emojis.spacer));
+}}
---
+++ Commands/Miscellaneous/links.js
@@ -0,0 +1,54 @@
+const Commando = require("discord.js-commando");
+const { MessageEmbed } = require("discord.js");
+const linksChannel = "742069780328087613";
+const { emojis } = require("../../config.json");
+
+module.exports = class LinkCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name: "link",
+            aliases: ["embed"],
+            group: "misc",
+            memberName: "link",
+            description: "Embed a link.",
+            args: [
+                {
+                    key: "title",
+                    prompt: "What would you like the title to be?",
+                    type: "string"
+                },
+                {
+                    key: "description",
+                    prompt: "Paste the link here!",
+                    type: "string"
+                }
+            ]
+        });
+    }
+
+    async run(message, { title, description }) {
+        if (message.channel.id === "745410767574007811") {
+            const embed = new MessageEmbed()
+            .setTitle(title)
+            .setDescription(description)
+            .setColor(15849719);
+
+            const { guild } = message;
+            const channel = guild.channels.cache.get(linksChannel);
+            channel.send(embed);
+        
+            const replyEmbed = new MessageEmbed()
+            .setTitle(`Sucessfully created embed!`)
+            .setDescription(`Your embed can be found on ${channel}!`)
+            .setColor(15849719);
+            message.channel.send(replyEmbed);
+        }
+        else if (message.channel.id === "760621183564513312") {
+            const faqEmbed = new MessageEmbed()
+            .setTitle(title)
+            .setDescription(description)
+            .setColor(15849719);
+            message.channel.send(faqEmbed)
+            .then (message.channel.send(emojis.spacer));
+        }
+}}
