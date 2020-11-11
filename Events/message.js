28713fd8383f01fb99213d191d17f4a362aedc1c
anomalilies <takitafu8@gmail.com>
2020-11-11 23:44:08 +0000

    Update message.js

--- Events/message.js
+++ Events/message.js
@@ -256,7 +256,7 @@ module.exports = async (client, message) => {
     // 2-Word Story Channel
     if (message.channel.id === "776229267998375946") {
         const args = message.content.trim().split(/ +/g);
-        if (!args[1] && args[2]) {
+        if (!args[0] && args[2]) {
             message.delete();
         }
     }
