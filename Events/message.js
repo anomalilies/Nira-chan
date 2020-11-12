d5478c590b64fbf607a3ee44a504054a834201e0
anomalilies <takitafu8@gmail.com>
2020-11-12 00:59:11 +0000

    Update message.js

--- Events/message.js
+++ Events/message.js
@@ -256,17 +256,17 @@ module.exports = async (client, message) => {
     // 2-Word Story Channel
     if (message.channel.id === "776229267998375946") {
         const args = message.content.trim().split(/ +/g);
-        const channel = client.channels.cache.get("776229267998375946");
+        /*const channel = client.channels.cache.get("776229267998375946");
 
         channel.messages.fetch({ limit: 1 }).then(messages => {
             let fetchedMsg = messages.first();
             if (message.author === fetchedMsg.author) {
                 message.delete()
             }
-            else if (!args[1] || args[2]) {
+            else*/if (!args[1] || args[2]) {
                 message.delete();
             }
-        })
+        //})
     }
 
     // Server Rules
