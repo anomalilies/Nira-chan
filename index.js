58c58fcb972d8fbeace07d8442e27b34f34d1c7e
anomalilies <takitafu8@gmail.com>
2020-11-21 04:30:29 +0000

    Starboard
    Don't star messages in starboard

--- index.js
+++ index.js
@@ -73,6 +73,7 @@ client.on("messageReactionAdd", async (reaction, user) => {
         }
     }
     if(reaction.emoji.name === "⭐") {
+        if (starboard) return;
         if(reaction.message.partial) {
             await reaction.fetch();
             await reaction.message.fetch();
