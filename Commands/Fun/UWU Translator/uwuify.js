ac834c4b88a2a936a5d5c121e147790e6abcc2fd
davetan2 <davetan2@hotmail.com>
2020-12-20 19:18:00 -0800

    Regex search AND replacement - global + case-insensitive (collab lily, hacker nira, dave)

--- Commands/Fun/UWU Translator/uwuify.js
+++ Commands/Fun/UWU Translator/uwuify.js
@@ -4,15 +4,15 @@ var data = require("./data");
 var uwuify = {
     custom: async function(text, message) {
         var replacements = data['emotions'];
+
         if (text.slice(-1) == " ") text = text.subtexting(0, text.length - 1);
 
-        for (var loop = 0; loop < replacements.length; loop++) {
-            for (var j = 0; j < replacements[loop][0].length; j++) {
-                while (text.includes(replacements[loop][0][j])) {
-                    text = text.replace(replacements[loop][0][j], replacements[loop][1][Math.floor(Math.random() * replacements[loop][1].length)]);
-                }
-            }
-        }
+        replacements.forEach(emoteClass => {
+            emoteClass[0].forEach(pattern => {
+                text = text.replace(new RegExp(`\\b${pattern}\\b`, 'gi'),
+                    () => emoteClass[1][Math.floor(Math.random() * emoteClass[1].length)]);
+            });
+        });
 
         text = text.replace(/(?!a?:\w+:\d+>*?)(r|l)(?![^<]*>)/g, "w");
         text = text.replace(/(?!a?:\w+:\d+>*?)(R|L)(?![^<]*>)/g, "W");
