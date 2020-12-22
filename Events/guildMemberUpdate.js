c8325914a04389f4442f9795f66aaf9015c5071a
anomalilies <takitafu8@gmail.com>
2020-12-22 03:47:04 +0000

    Update guildMemberUpdate.js

--- Events/guildMemberUpdate.js
+++ Events/guildMemberUpdate.js
@@ -36,7 +36,7 @@ module.exports = async (client, oldMember, newMember) => {
         else if (oldMember.roles.cache.has(isContributor) && !newMember.roles.cache.some(inContributorGroup)) {
             newMember.roles.remove(isContributor);
         }
-        // Mute Role
+        /*// Mute Role
         if (oldMember.roles.cache.has(isVIP) && newMember.roles.cache.has(isMute)) {
             newMember.roles.remove(isVIP); // If muted, remove V.I.P.
         }
@@ -48,6 +48,6 @@ module.exports = async (client, oldMember, newMember) => {
         }
         else if (oldMember.roles.cache.has(isMute) && !newMember.roles.cache.has(isMute)) {
             newMember.roles.add(isVIP); // If unmuted, add V.I.P.
-        }
+        }*/
     }
 };
