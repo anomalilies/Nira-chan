eb52a8dfbc611d27fad693d4b5b0f4f6ef705ea4
anomalilies <takitafu8@gmail.com>
2020-12-27 21:15:18 +0000

    this doesn't

--- Embeds/Role-Picker/rolepickerCommands.js
+++ Embeds/Role-Picker/rolepickerCommands.js
@@ -6,19 +6,48 @@ module.exports = async (client, id = []) => {
     channel.messages.fetch().then((messages) => {
         const niraMessages = messages.filter(msg => msg.author == client.user);
 
-        const reactions = {           
-            coloursReact: ["🦑"], // ["781294389241970688", "781295090617811004", "781295102046765066", "783501246954340382", "781295124280901643", "781295137635958804"]
-            dividersReact: ["764025729696268319", "756742977971552286", "764027219323912202", "764248315553775656", "764026501066129408", "777269746722668565"],
-            miscReact: ["742096993731477505", "742096470462824468", "742090483446317107"],
-            pronounsReact: ["755500124091973703", "742130938468630659", "750462152199897208", "742104449660747908"]
+        const data = {
+            colours: {
+                roles: {
+                    // "reaction id": "role id"
+                    "781294389241970688": "745380089457410192",
+                    "781295090617811004": "760695283871907841",
+                    "781295102046765066": "760695285000831017",
+                    "783501246954340382": "760695501447889016",
+                    "781295124280901643": "760695751499579504",
+                    "781295137635958804": "752308894474174515"
+                },
+                hasPermission: (user, role) => true
+            }, dividers: {
+                roles: {
+                    "764025729696268319": "770127096194269225",
+                    "756742977971552286": "770119715808477244",
+                    "764027219323912202": "770310986275618827",
+                    "764248315553775656": "770310988938346526",
+                    "764026501066129408": "781322220382453780",
+                    "777269746722668565": "781322360967266344",
+                },
+                hasPermission: (user, role) => true
+            }, misc: {
+                roles: {
+                    "742096993731477505": "772657659635171348",
+                    "742096470462824468": "758482374232506397",
+                    "742090483446317107": "753248752332046467"
+                },
+                hasPermission: (user, role) => true
+            }, pronouns: {
+                roles: {
+                    "755500124091973703": "742068285553770529",
+                    "742130938468630659": "742068282777141369",
+                    "750462152199897208": "742068286719524865",
+                    "742104449660747908": "772539432515534890"
+                },
+                hasPermission: (user, role) => true
+            }
         }
-
-        const roles = {
-            coloursID: ["745380089457410192", "760695283871907841", "760695285000831017", "760695501447889016", "760695751499579504", "752308894474174515"],
-            dividersID: ["770127096194269225", "770119715808477244", "770310986275618827", "770310988938346526", "781322220382453780", "781322360967266344"],
-            miscID: ["772657659635171348", "758482374232506397", "753248752332046467"],
-            pronounsID: ["742068285553770529", "742068282777141369", "742068286719524865", "772539432515534890"]
-        }
+        const map = new Map();
+        map.set(data)
+        console.log(map)
 
         if (niraMessages.size === 0) {
             channel.send(pronouns)
@@ -28,7 +57,8 @@ module.exports = async (client, id = []) => {
             channel.send(dividers);
             channel.send(emojis.spacer);
             channel.send(colours);
-        } else {
+        }
+        else {
             const embedMessages = niraMessages.filter(msg => !msg.content.startsWith(emojis.spacer));
             let arr = Object.values(reactions);
             var x;
@@ -38,7 +68,6 @@ module.exports = async (client, id = []) => {
                 }
             }
 
-            // Pronouns
             const pronounMessage = embedMessages.array()[3]
             const pronounReactionCollector = pronounMessage.createReactionCollector((reaction, user) => {
                 return reactions.pronounsReact.includes(reaction.emoji.id)
