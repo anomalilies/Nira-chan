7f7d32707dd3212d8513a00e76e6426197eed78a
anomalilies <takitafu8@gmail.com>
2021-01-03 18:57:56 +0000

    Update kirby.js

--- Commands/Fun/kirby.js
+++ Commands/Fun/kirby.js
@@ -17,18 +17,30 @@ module.exports = class KirbyCommand extends Commando.Command {
     }
 
     async run(message) {
-        if (message.channel.type === "dm" || allowlists.botspamchannels.includes(message.channel.id) || message.guild.id !== homeguild) {
-            const index = Math.floor(Math.random() * abilities.length);
-            
-            const embed = new MessageEmbed()
-            .setAuthor(message.author.tag, message.author.displayAvatarURL({dynamic:true}))
-            .setColor(15849719);
+        if (message.channel.type === "dm" || allowlists.botspamchannels.includes(message.channel.id) || message.guild.id !== homeguild || message.member.roles.cache.get("752308894474174515")) {
+            for(var key in abilities){
+                var abilityGroup = abilities[key]
+            }
+            var weights = [45, 20, 10, 5, 4, 4, 3, 3, 2, 2, 1, 1]
 
+            var total = 0;
+            for(var i = 0, len = weights.length; i < len; i++) {
+                weights[i] = [total, total += weights[i]];
+            }
+            var randomNumber = parseInt(Math.random() * total);
+            for(;randomNumber < weights[--i][0];) {
+                var ability = abilityGroup[i];
+            }
+            const index = Math.floor(Math.random() * ability.length);
+
+            const embed = new MessageEmbed()
+                .setAuthor(message.author.tag, message.author.displayAvatarURL({dynamic:true}))
+                .setColor(15849719);
             if (message.channel.type === "dm") {
-                embed.setDescription(`<:kirbsucc:757289104789471322> Kirby inhaled **${message.author.username}** and got the **${abilities[index]}** ability!`)
+                embed.setDescription(`<:kirbsucc:757289104789471322> Kirby inhaled **${message.author.username}** and got the **${ability[index]}** ability!`);
             } else {
                 let member = message.guild.member(message.author);
-                embed.setDescription(`<:kirbsucc:757289104789471322> Kirby inhaled **${member.displayName}** and got the **${abilities[index]}** ability!`)
+                embed.setDescription(`<:kirbsucc:757289104789471322> Kirby inhaled **${member.displayName}** and got the **${ability[index]}** ability!`);
             }
             message.channel.send(embed);
         }
