99065adf888effbc71d7b4a1e273dbf3724f697a
anomalilies <takitafu8@gmail.com>
2021-01-06 02:31:54 +0000

    Kirby Upgrade!

--- Commands/Fun/kirby.js
+++ Commands/Fun/kirby.js
@@ -18,30 +18,52 @@ module.exports = class KirbyCommand extends Commando.Command {
 
     async run(message) {
         if (message.channel.type === "dm" || allowlists.botspamchannels.includes(message.channel.id) || message.guild.id !== homeguild || message.member.roles.cache.get("752308894474174515")) {
-            for(var key in abilities){
-                var abilityGroup = abilities[key]
-            }
-            var weights = [45, 20, 10, 5, 4, 4, 3, 3, 2, 2, 1, 1]
+            var abilityGroup = [];
+            var weights = [];
 
-            var total = 0;
-            for(var i = 0, len = weights.length; i < len; i++) {
-                weights[i] = [total, total += weights[i]];
+            for (var key in abilities) {
+                abilityGroup.push(abilities[key].ability);
+                weights.push(abilities[key].weight);
             }
-            var randomNumber = parseInt(Math.random() * total);
-            for(;randomNumber < weights[--i][0];) {
-                var ability = abilityGroup[i];
+            var i, nickname = "";
+            for (i = 0; i < weights.length; i++) {
+                weights[i] += weights[i - 1] || 0;
             }
+            var random = Math.random() * weights[weights.length - 1];
+            for (i = 0; i < weights.length; i++) {
+                if (weights[i] > random) {
+                    break;
+                }
+            }
+            var ability = abilityGroup[i]
             const index = Math.floor(Math.random() * ability.length);
+
+            if (message.channel.type === "dm") {
+                var nickname = message.author.username;
+            }
+            else {
+                let member = message.guild.member(message.author);
+                var nickname = member.displayName;
+            }
+            const replies = [
+                `Kirby inhaled **${nickname}** and got the **${ability[index]}** ability!`,
+                `Somehow, when Kirby inhaled **${nickname}**, he got the **${ability[index]}** ability!`,
+                `Elemental combo! Kirby got the **${ability[index]}** ability when inhaling **${nickname}**!`,
+                `Power combo! Kirby got the **${ability[index]}** ability when inhaling **${nickname}**!`,
+                `Woah! Kirby got the **${ability[index]}** super ability when inhaling **${nickname}**!`,
+                `Kirby inhaled **${nickname}** and... Turned into yarn? He uses the **${ability[index]}** attack!`,
+                `Kirby found a Robobot Armour docking station! After inhaling **${nickname}**, he uses **${ability[index]}**!`,
+                `With **${nickname}**, Kirby partners up with his friends! He uses the **${ability[index]}** attack!`,
+                `Wow! Kirby inhaled **${nickname}** and got the rare **${ability[index]}** ability!`,
+                `Kirby inhaled **${nickname}** and... Turned into yarn? He gains the **${ability[index]}** ability!`,
+                `As Kirby is approaching an impending boss fight, anxiety creeps over him. No need to worry, though; Kirby inhales **${nickname}**, and gets the **${ability[index]}** ability!`,
+                `No way... Kirby inhaled **${nickname}** and got the ultra-rare **${ability[index]}** ability!`
+            ]
 
             const embed = new MessageEmbed()
                 .setAuthor(message.author.tag, message.author.displayAvatarURL({dynamic:true}))
-                .setColor(15849719);
-            if (message.channel.type === "dm") {
-                embed.setDescription(`<:kirbsucc:757289104789471322> Kirby inhaled **${message.author.username}** and got the **${ability[index]}** ability!`);
-            } else {
-                let member = message.guild.member(message.author);
-                embed.setDescription(`<:kirbsucc:757289104789471322> Kirby inhaled **${member.displayName}** and got the **${ability[index]}** ability!`);
-            }
+                .setColor(15849719)
+                .setDescription(`<:kirbsucc:757289104789471322> ${replies[i]}`);
             message.channel.send(embed);
         }
     }
