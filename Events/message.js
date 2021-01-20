ef06202ccbee16a89ae0cfd50bf05b082b50e809
anomalilies <takitafu8@gmail.com>
2021-01-20 02:11:53 +0000

    Update message.js

--- Events/message.js
+++ Events/message.js
@@ -211,7 +211,7 @@ module.exports = async (client, message) => {
                     if (emoji.name === group1) {
                         // We only need to resend if we replace any animated emoji
                         // But don't make the variable false if it's already true
-                        needs_resend = emoji.animated || needs_resend || emoji.guild;
+                        needs_resend = emoji.animated || needs_resend || !emoji.guild;
                         let type = emoji.animated ? "a" : "";
                         replaceString = `<${type}:${emoji.name}:${emoji.id}>`;
                     }
@@ -231,7 +231,7 @@ module.exports = async (client, message) => {
     if (message.guild && message.content[0] === prefix) {
         guilds.forEach(guild => {
             guild.emojis.cache.each(async emoji => {
-                if (message.content === `${prefix}${emoji.name}` && emoji.animated) {
+                if (message.content === `${prefix}${emoji.name}` && (emoji.animated || !emoji.guild)) {
                     await replaceMessageThroughWebhook(message, `<a:${emoji.name}:${emoji.id}>`);
                 }
             });
