0f9eaf61e12d3f173c44fcc4bf1b8ec0981497e0
kanon <68348089+anomalilies@users.noreply.github.com>
2021-01-23 23:28:02 +0000

    magic

--- Events/guildMemberAdd.js
+++ Events/guildMemberAdd.js
@@ -1,10 +1,12 @@
 const configFileName = process.env.NIRA_DEV ? 'config.dev.json' : 'config.json';
-const { homeguild } = require(`../${configFileName}`);
+const { homeguild, members } = require(`../${configFileName}`);
 
 module.exports = (client, member) => {
-    const channel = client.channels.cache.get("742513756059467917");
+    const modlog = client.channels.cache.get("742513756059467917");
+    const channel = client.channels.cache.get("603246092402032673");
 
     if (member.guild.id === homeguild) {
-        channel.send("**" + member.user.username + "** joined! <:niraHello:777736555829002281>");
+        modlog.send(`**${member.user.username}** joined! <:niraHello:777736555829002281>`);
+        channel.send(`<@${members.currentowner}> tells me that **${member.user.username}** will join shortly... 🪄`)
     }
 };
--- Events/guildMemberRemove.js
+++ Events/guildMemberRemove.js
@@ -2,9 +2,9 @@ const configFileName = process.env.NIRA_DEV ? 'config.dev.json' : 'config.json';
 const { homeguild } = require(`../${configFileName}`);
 
 module.exports = (client, member) => {
-    const channel = client.channels.cache.get("742513756059467917");
+    const modlog = client.channels.cache.get("742513756059467917");
 
     if (member.guild.id === homeguild) {
-        channel.send("**" + member.user.username + "** left... <:niraWail:777736598854696980>");
+        modlog.send(`**${member.user.username}** left... <:niraWail:777736598854696980>`);
     }
 };
