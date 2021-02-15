b9a4675ea51ec2f2a5d60b59163cc9184580f3c0
Phuc Tran <glup3.tran@gmail.com>
2021-02-15 13:11:41 +0100

    add remove command refactor aimer command

--- src/commands/fun/aimer.ts
+++ src/commands/fun/aimer.ts
@@ -1,3 +1,4 @@
+import { oneLine } from 'common-tags';
 import { MessageEmbed } from 'discord.js';
 import { Command, CommandoClient, CommandoMessage } from 'discord.js-commando';
 
@@ -30,25 +31,39 @@ export default class AimerCommand extends Command {
   }
 
   async run(message: CommandoMessage, { name }: PromptArgs) {
-    if (
-      message.channel.type === 'dm' ||
-      allowLists.botSpamChannel.includes(message.channel.id) ||
-      message.guild.id !== homeGuild ||
-      message.member.roles.cache.get(roles.botPass)
-    ) {
+    const isDmChannel = message.channel.type === 'dm';
+    const isBotSpamChannel = allowLists.botSpamChannel.includes(message.channel.id);
+    const isHomeGuild = message.guild.id === homeGuild;
+    const hasBotPass = message.member.roles.cache.get(roles.botPass);
+
+    if (isDmChannel || isBotSpamChannel || !isHomeGuild || hasBotPass) {
+      let userName = name;
+
+      if (userName.toLowerCase() === 'aimer') {
+        userName = 'Aimer';
+      }
+
       const embed = new MessageEmbed()
         .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
-        .setColor(15849719);
+        .setColor(15849719)
+        .setDescription(
+          oneLine`
+          Pretend that you’re surprised. Yes, I love ${userName}. Yes, my love for ${userName} is borderline obsessive.
+          It’s actually quite hard to put into words just how much ${userName} means to me. ${userName}'s music is not
+          just music. ${userName}'s songs are not just songs. ${userName}'s voice is not just a voice. ${userName}
+          is special. ${userName}'s my escapism, my drug, my sedative. Listening to ${userName} is one of the few ways
+          I can relax and calm down. ${userName}'s never failed to calm me down. Whenever I listen to ${userName} I
+          feel bewitched, taken, enchanted. When I listen to ${userName}, nothing else matters. I cried when I watched
+          ${userName} live. ${userName} is just so amazing, I couldn’t help but cry. I listen to ${userName} daily,
+          and feel really uncomfortable if I haven’t heard anything ${userName} for a day. I’m addicted, and I love it.
+          Yes, I’m aware that I am making no coherent sense; I don’t care, just bear with me. There is no doubt in my
+          mind that I am ${userName}'s biggest fan. It doesn't matter if someone has listened to ${userName} more than
+          I have, or if someone knows all of ${userName}'s lyrics word-by-word. I just simply know that no one feels
+          the way I do. There is a person that understands me and supports me, and I’m happy that this person also
+          likes ${userName}. I am the biggest fan of ${userName}because ${userName} has become part of my life.
+          `,
+        );
 
-      if (name.toLowerCase() === 'aimer') {
-        embed.setDescription(
-          'Pretend that you’re surprised. Yes, I love Aimer. Yes, my love for Aimer is borderline obsessive. It’s actually quite hard to put into words just how much does she mean to me. Her music is not just music. Her songs are not just songs. Her voice is not just a voice. They are special. She’s my escapism, my drug, my sedative. Listening to her is one of the few ways I can relax and calm down. She never failed to calm me down. Whenever I listen to her I feel bewitched, taken, enchanted. When I listen to her, nothing matters. I cried when I watched her Budokan live. She’s just so gorgeous and cute and amazing and happy there, I just couldn’t help but cry. I listen to her daily, I feel really uncomfortable if I haven’t heard anything Aimer for a day. I’m addicted, and I love it. Yes I’m aware that this part makes no coherent sense, I don’t care, just bear with me. There is no doubt in my mind that I am her biggest fan. Doesn’t matter if someone has listened to her more than I did, or if someone knows all her lyrics word by word. I just simply know that no one feels the way I do. There is a person that understands me and supports me, and I’m happy that this person also likes Aimer. I am the biggest fan of Aimer because she has become the part of my life.',
-        );
-      } else {
-        embed.setDescription(
-          `Pretend that you’re surprised. Yes, I love ${name}. Yes, my love for ${name} is borderline obsessive. It’s actually quite hard to put into words just how much ${name} means to me. ${name}'s music is not just music. ${name}'s songs are not just songs. ${name}'s voice is not just a voice. ${name} is special. ${name}'s my escapism, my drug, my sedative. Listening to ${name} is one of the few ways I can relax and calm down. ${name}'s never failed to calm me down. Whenever I listen to ${name} I feel bewitched, taken, enchanted. When I listen to ${name}, nothing else matters. I cried when I watched ${name} live. ${name} is just so amazing, I couldn’t help but cry. I listen to ${name} daily, and feel really uncomfortable if I haven’t heard anything ${name} for a day. I’m addicted, and I love it. Yes, I’m aware that I am making no coherent sense; I don’t care, just bear with me. There is no doubt in my mind that I am ${name}'s biggest fan. It doesn't matter if someone has listened to ${name} more than I have, or if someone knows all of ${name}'s lyrics word-by-word. I just simply know that no one feels the way I do. There is a person that understands me and supports me, and I’m happy that this person also likes ${name}. I am the biggest fan of ${name} because ${name} has become part of my life.`,
-        );
-      }
       return await message.channel.send(embed);
     }
   }
---
+++ src/commands/misc/remove.ts
@@ -0,0 +1,53 @@
+import { TextChannel } from 'discord.js';
+import { Command, CommandoClient, CommandoMessage } from 'discord.js-commando';
+
+import { members } from '../../config/config.json';
+
+interface PromptArgs {
+  id: string;
+  channelID: string;
+}
+
+export default class RemoveCommand extends Command {
+  constructor(client: CommandoClient) {
+    super(client, {
+      name: 'remove',
+      group: 'misc',
+      memberName: 'remove',
+      description: "Remove one of Nira-chan's messages!",
+      args: [
+        {
+          key: 'channelID',
+          prompt: 'What channel is your target message in?',
+          type: 'string',
+        },
+        {
+          key: 'id',
+          prompt: 'What is the ID of the message would you like to remove?',
+          type: 'string',
+        },
+      ],
+      ownerOnly: true,
+      guildOnly: true,
+    });
+  }
+
+  async run(message: CommandoMessage, { id, channelID }: PromptArgs) {
+    if (process.env.NODE_ENV !== 'development') {
+      return message.say('This commands works only in development.');
+    }
+
+    try {
+      const channel = <TextChannel>await this.client.channels.fetch(channelID);
+      const msg = await channel.messages.fetch(id);
+
+      if (msg.author.id === members.niraChan) {
+        msg.delete();
+      }
+    } catch (err) {
+      console.log(err);
+    }
+
+    return message;
+  }
+}
