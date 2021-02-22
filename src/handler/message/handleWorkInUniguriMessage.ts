78c4bc21dc6d4dfd3bb26dc5dd38fc7b7dfeaa83
Phuc Tran <glup3.tran@gmail.com>
2021-02-22 22:32:48 +0100

    dont delete bot messages

--- src/handler/message/handleWorkInUniguriMessage.ts
+++ src/handler/message/handleWorkInUniguriMessage.ts
@@ -5,12 +5,18 @@ import { isInChannel } from '../../util/checks';
 import { workInUniguriMessage } from '../../config/event_handler.json';
 import { keyv } from '../../database/keyv';
 
+const unbelievaBot = '292953664492929025';
+
 export const handleWorkInUniguriMessage = async (message: CommandoMessage) => {
   if ((await keyv.get(Object.keys({ workInUniguriMessage })[0])) === false) {
     return;
   }
 
   if (!isInChannel(message, allChannels.uniguri)) {
+    return;
+  }
+
+  if (message.author.id === unbelievaBot) {
     return;
   }
 
