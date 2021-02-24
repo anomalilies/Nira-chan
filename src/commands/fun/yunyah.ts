086ed8eb97362859cbb3861afce4953cc73ee24b
anomalilies <takitafu8@gmail.com>
2021-02-24 23:44:07 +0000

    yunyah

---
+++ src/commands/fun/yunyah.ts
@@ -0,0 +1,44 @@
+import { MessageEmbed, TextChannel } from 'discord.js';
+import { Command, CommandoClient, CommandoMessage } from 'discord.js-commando';
+
+import { doesUserHaveBotpass, isBotspamChannel, isDmChannel, isHomeGuild } from '../../util/checks';
+import yunyahQuotes from '../../data/yunyah.json';
+
+export default class YunyahCommand extends Command {
+  constructor(client: CommandoClient) {
+    super(client, {
+      name: 'yunyah',
+      group: 'fun',
+      memberName: 'yunyah',
+      description: `Is ACA NEE?`,
+    });
+  }
+
+  async run(message: CommandoMessage) {
+    const quote = yunyahQuotes[Math.floor(Math.random() * yunyahQuotes.length)];
+
+    if (isDmChannel(message)) {
+      const embed = new MessageEmbed({
+        author: { name: message.author.tag, iconURL: message.author.displayAvatarURL({ dynamic: true }) },
+        description: quote,
+        color: '#F1D8F7',
+      });
+      await message.channel.send(embed);
+    } else if (isBotspamChannel(message) || !isHomeGuild(message) || doesUserHaveBotpass(message)) {
+      const webhooks = await (<TextChannel>message.channel).fetchWebhooks();
+      let webhook = webhooks.first();
+
+      if (!webhook) {
+        webhook = await (<TextChannel>message.channel).createWebhook('Nira-chan');
+      }
+      message.delete();
+
+      await webhook.send(quote, {
+        username: message.member.displayName,
+        avatarURL: message.author.displayAvatarURL(),
+        files: message.attachments.array(),
+      });
+    }
+    return message;
+  }
+}
