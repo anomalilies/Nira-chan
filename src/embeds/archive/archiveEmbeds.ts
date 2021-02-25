0ec2a0111377a426bbe7f274b078783a70946dd6
anomalilies <takitafu8@gmail.com>
2021-02-25 06:19:32 +0000

    gn

--- src/embeds/archive/archiveEmbeds.ts
+++ src/embeds/archive/archiveEmbeds.ts
@@ -11,24 +11,15 @@ export const archive = new MessageEmbed({
   fields: [
     {
       name: 'Server Archive',
-      value: oneLine`
-        [Google Drive Folder]
-        (https://drive.google.com/drive/folders/16RLTkFCgZcVZPYwYDTdxDJxknQUC_Ll2?usp=sharing)
-      `,
+      value: `[Google Drive Folder](https://drive.google.com/drive/folders/16RLTkFCgZcVZPYwYDTdxDJxknQUC_Ll2?usp=sharing)`,
     },
     {
       name: '2020 Bracket Results',
-      value: oneLine`
-        [Direct Download Link]
-        (https://cdn.discordapp.com/attachments/753827379427672084/775617580848381972/ZUTOMAYO_Bracket_-_October_2020.html)
-      `,
+      value: `[Direct Download Link](https://cdn.discordapp.com/attachments/753827379427672084/775617580848381972/ZUTOMAYO_Bracket_-_October_2020.html)`,
     },
     {
       name: "Server 'Lore'",
-      value: oneLine`
-        [Google Docs Link]
-        (https://docs.google.com/document/d/1F5kUSwy7uMkdnKjuacZvJR1XeksLSsU8GweLcxynrCY/edit?usp=sharing)
-      `,
+      value: `[Google Docs Link](https://docs.google.com/document/d/1F5kUSwy7uMkdnKjuacZvJR1XeksLSsU8GweLcxynrCY/edit?usp=sharing)`,
     },
     {
       name: 'J-Music Archive',
@@ -46,8 +37,5 @@ export const nitro = new MessageEmbed({
     Are you a Nitro user, looking for more ZUTOMAYO emojis?
     Check out our affiliates!
   `,
-  fields: [
-    { name: 'Server of Grey', value: '[Invite](https://discord.gg/p9Vg6xtdk6) | Owned by <@394724520872771585>' },
-    { name: "Lily's Test Server", value: '[Invite](https://discord.gg/n7YcF3EDAk)' },
-  ],
+  fields: [{ name: "Lily's Test Server", value: '[Invite](https://discord.gg/n7YcF3EDAk)' }],
 });
