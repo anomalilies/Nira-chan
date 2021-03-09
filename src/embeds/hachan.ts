98ce957ea09fa2efdb457210374803b912a57bfb
anomalilies <takitafu8@gmail.com>
2021-03-09 19:41:37 +0000

    jajajajax4000

--- src/embeds/hachan.ts
+++ src/embeds/hachan.ts
@@ -2,9 +2,7 @@ import { TextChannel } from 'discord.js';
 import { CommandoClient } from 'discord.js-commando';
 
 export const hachan = async (client: CommandoClient) => {
-  const targetGuild = client.guilds.cache.get('764936595966918706');
-  const targetChannel = targetGuild.channels.cache.find((channel: any) => channel.name.toLowerCase() === 'nira-chan');
-  console.log(targetChannel);
-  (targetChannel as TextChannel).send('JAJAJAJAJAJAJA');
+  const channel = <TextChannel>client.channels.cache.get('786511408035528734');
+  channel.send('JAJAJAJAJAJAJA');
   return;
 };
