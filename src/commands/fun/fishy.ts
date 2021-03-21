3adde2483673a503602963d0ddefcca057044ce4
anomalilies <68348089+anomalilies@users.noreply.github.com>
2021-03-21 06:07:51 +0000

    help

--- src/commands/fun/fishy.ts
+++ src/commands/fun/fishy.ts
@@ -2,8 +2,8 @@
 import { MessageEmbed } from 'discord.js';
 import { PrismaClient } from '@prisma/client';
 import { Command, CommandoClient, CommandoMessage } from 'discord.js-commando';
-//import { isInChannel, isDmChannel, isHomeGuild } from '../../util/checks';
-import { /*allChannels,*/ prefix } from '../../config/config.json';
+import { isInChannel, isDmChannel, isHomeGuild } from '../../util/checks';
+import { allChannels, prefix } from '../../config/config.json';
 
 const prisma = new PrismaClient();
 import fish from '../../data/fish.json';
@@ -23,7 +23,7 @@ export default class FishyCommand extends Command {
           type: 'string',
           default: '',
           validate: (name: string) => {
-            if (name.match(/(<@!\d{18}>)/)) return true;
+            if (name.match(/(^<@!\d{18}>$)/)) return true;
             return 'Invalid user!';
           },
         },
@@ -51,60 +51,78 @@ export default class FishyCommand extends Command {
           userId: user.id,
         },
       });
-
-      if (hasFished === null) {
-        await prisma.fishy.create({
-          data: { userId: user.id },
-        });
-        console.log(`Added ${user.username} to database!`);
-      }
-      console.log(hasFished);
-
-      //if (isDmChannel(message) || isInChannel(message, allChannels.fishy) || !isHomeGuild(message)) {
-      const total = fish.reduce((acc, cur) => acc + cur.weight, 0);
-      const threshold = Math.random() * total;
-
-      let sum = 0;
-      const group = fish.find((group) => {
-        sum += group.weight;
-        return sum >= threshold;
-      });
-
-      const index = Math.floor(Math.random() * group.puns.length);
-      const fishPun = group.puns[index];
-
-      if (group.catch.startsWith('Caught trash!')) {
-        var reply = group.catch;
-      } else {
-        var amount = Math.floor(Math.random() * (group.max - group.min) + group.min);
-        var reply = group.catch.replace('{amount}', amount.toString());
-
-        var newTotal = await prisma.fishy.update({
-          where: {
-            userId: user.id,
-          },
-          data: {
-            totalFish: +amount, //og totalfish + amount
-          },
-        });
-      }
-
-      const embed = new MessageEmbed({
-        title: reply,
-        color: '#F1D8F7',
-        fields: [
-          {
-            name: 'Mr. Fish says...',
-            value: `> ${fishPun}`,
-          },
-        ],
-        footer: {
-          text: `${user.tag} has ${newTotal.totalFish} fishy`,
-          iconURL: user.displayAvatarURL({ dynamic: true }),
+      const initialFisher = await prisma.fishy.findUnique({
+        where: {
+          userId: message.author.id,
         },
       });
-      return await message.channel.send(embed.setTimestamp());
-      //}
+
+      const loop = [
+        { property: initialFisher, id: message.author.id },
+        { property: hasFished, id: user.id },
+      ];
+      for (const key in loop) {
+        if (loop[key].property === null) {
+          await prisma.fishy.create({
+            data: { userId: loop[key].id },
+          });
+          console.log(`Added ${user.username} to database!`);
+        }
+      }
+
+      if (
+        (initialFisher.lastFish === null ||
+          hasFished.lastFish === null ||
+          Date.now() >= initialFisher.lastFish.getTime() + 720/*0000*/) &&
+        (isDmChannel(message) || isInChannel(message, allChannels.fishy) || /*!*/isHomeGuild(message))
+      ) {
+        const total = fish.reduce((acc, cur) => acc + cur.weight, 0);
+        const threshold = Math.random() * total;
+
+        let sum = 0;
+        const group = fish.find((group) => {
+          sum += group.weight;
+          return sum >= threshold;
+        });
+
+        const index = Math.floor(Math.random() * group.puns.length);
+        const fishPun = group.puns[index];
+
+        if (group.catch.startsWith('Caught trash!')) {
+          var reply = group.catch;
+          var amount = 0;
+        } else {
+          var amount = Math.floor(Math.random() * (group.max - group.min) + group.min);
+          var reply = group.catch.replace('{amount}', amount.toString());
+
+          var newTotal = await prisma.fishy.update({
+            where: {
+              userId: user.id,
+            },
+            data: {
+              totalFish: hasFished.totalFish + amount,
+            },
+          });
+        }
+
+        const embed = new MessageEmbed({
+          title: reply,
+          color: '#F1D8F7',
+          fields: [
+            {
+              name: 'Mr. Fish says...',
+              value: `> ${fishPun}`,
+            },
+          ],
+          footer: {
+            text: `${user.tag} has ${newTotal.totalFish} fishy`,
+            iconURL: user.displayAvatarURL({ dynamic: true }),
+          },
+        });
+        return await message.channel.send(embed.setTimestamp());
+      } else {
+        message.channel.send('fishy timer goes here...');
+      }
     } else {
       message.channel.send('Database max reached!');
     }
