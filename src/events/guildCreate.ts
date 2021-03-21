236beeca1578934fd9b4bbcd09e49c23763e4026
anomalilies <68348089+anomalilies@users.noreply.github.com>
2021-03-21 21:03:43 +0000

    finally fixed bug?

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -48,6 +48,7 @@ export default async function (client: CommandoClient, guild: Guild) {
           }
         } else {
           guild.owner.send(embed);
+          found = 1;
         }
         return;
       }
