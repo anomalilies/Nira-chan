68e983099b2f70d555445687d3439bd0d2a9083b
anomalilies <takitafu8@gmail.com>
2021-07-14 21:18:03 +0100

    Updated welcome message

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -35,13 +35,29 @@ export default async function (client: CommandoClient, guild: Guild) {
         value:
           "Nira's main, and most-popular, functionality allows users to use emojis from any server the bot is in without a Nitro subscription.\nHowever, by using Nira, you agree to not upload **hateful, degoratory, or NSFW emojis**, if you choose to opt-in to this feature.\nAny such offending content found will result in the permanent blacklist of your server's emojis from being used globally, or all servers the owner has the `Manage Emojis` permission in.\n\n**To opt in to global emoji sharing, use `" +
           prefix +
-          'emojiauth`**; You can change these settings at any time.' +
-          `\nTo report an emoji, simply copy the ID, and contact ${members.currentOwner} directly, either via DM or the` +
+          'emojiauth`.**\nNote that you can change these settings at any time!' +
+          `\n\nTo report an emoji, simply copy the ID, and contact <@${members.currentOwner}> directly, either via DM or the` +
           '`' +
           prefix +
           'contact` command.\nAll emojis available for global use can be accessed using the `' +
           prefix +
           'emojilist` command.',
+        inline: false,
+      },
+      {
+        name: 'GitHub Repo',
+        value: `[Link](https://github.com/anomalilies/Nira-chan)`,
+        inline: true,
+      },
+      {
+        name: 'Discord Server',
+        value: `[Link](https://discord.gg/htSDkHH)`,
+        inline: true,
+      },
+      {
+        name: 'Ko-fi',
+        value: `[Link](https://ko-fi.com/uniguri)`,
+        inline: true,
       },
     ],
     color: colour,
