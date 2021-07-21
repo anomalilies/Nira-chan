f9a5bbd9c5c8daac78d1013b4a3d263898a69f7e
anomalilies <takitafu8@gmail.com>
2021-07-21 01:29:09 +0100

    Contact command

---
+++ src/commands/misc/contact.ts
@@ -0,0 +1,31 @@
+import { MessageEmbed } from 'discord.js';
+import { Command, CommandoClient, CommandoMessage } from 'discord.js-commando';
+import { colour, members } from '../../config/config.json';
+
+export default class ContactCommand extends Command {
+  constructor(client: CommandoClient) {
+    super(client, {
+      name: 'contact',
+      group: 'misc',
+      memberName: 'contact',
+      description: 'Get support in using Nira, and suggest new features.',
+    });
+  }
+
+  async run(message: CommandoMessage) {
+    const embed = new MessageEmbed({
+      title: `Support`,
+      color: colour,
+      description: `Need support in using <@!${this.client.user.id}>?\nContact <@!${members.currentOwner}> through DMs, or **through the support server found below**.`,
+      fields: [
+        {
+          name: 'Discord Server',
+          value: `[Link](https://discord.gg/htSDkHH)`,
+        },
+      ],
+      author: { name: message.author.tag, iconURL: message.author.displayAvatarURL({ dynamic: true }) },
+    });
+
+    return await message.channel.send(embed);
+  }
+}
